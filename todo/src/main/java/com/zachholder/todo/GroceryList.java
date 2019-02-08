010af7bff952f29e3e5e8086e0063a46c95168f3
Zach Holder <zacharysholder@gmail.com>
2019-02-07 21:05:52 -0600

    project name change 2

--- todo/src/main/java/com/zachholder/todo/GroceryList.java
+++ todo/src/main/java/com/zachholder/todo/GroceryList.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoApplication {
+public class GroceryList {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoApplication.class, args);
+		SpringApplication.run(GroceryList.class, args);
 	}
 }
 
