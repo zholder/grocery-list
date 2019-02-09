5612d9e9cd9e5d43525a8c5515962971e6d20ea3
Zach Holder <zacharysholder@gmail.com>
2019-02-09 09:33:01 -0600

    initial branch commit

--- todo/src/main/java/com/zachholder/todo/Todo.java
+++ todo/src/main/java/com/zachholder/todo/Todo.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroceryList {
+public class Todo {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroceryList.class, args);
+		SpringApplication.run(Todo.class, args);
 	}
 }
 
